2df80b862053862739452c53aca93c9bbf7786df
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-19 10:04:01 -0500

    Profile Structure

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import { Text, VStack } from "@chakra-ui/react"
+
+import banner from "../assets/banner.jpg"
+import profile from "../assets/profile.jpg"
+
+const Profile = () => {
+  return (
+    <VStack>
+      <Image src={banner} boxSize="full" />
+      <Image src={profile} boxSize="64px" borderRadius="full" />
+      <Text>Nestor Mamani</Text>
+      <Text>Web artisan 🦄</Text>
+    </VStack>
+  )
+}
+
+export default Profile
