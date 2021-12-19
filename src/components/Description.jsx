0935ddb3f1ac08e3025ed674a1ed6f001ba24000
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-19 10:12:33 -0500

    Description component start

---
+++ src/components/Description.jsx
@@ -0,0 +1,14 @@
+import { VStack, Text, Box } from "@chakra-ui/react"
+
+const Description = () => {
+  return (
+    <VStack>
+      <Text>Descripción</Text>
+      <Box>
+        <Text>Hola, soy Nestor</Text>
+      </Box>
+    </VStack>
+  )
+}
+
+export default Description
