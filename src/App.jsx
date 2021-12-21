998263ef38b103d4f947d392c53f8359e15e4881
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-20 20:03:56 -0500

    Wave function

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { Button, Grid } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
-import Header from "./components/Header"
-import Main from "./components/Main"
+import { Button, Grid } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+import Header from './components/Header'
+import Main from './components/Main'
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("")
+  const [currentAccount, setCurrentAccount] = useState('')
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window
       if (!ethereum) {
-        alert("Make sure you have Metamask")
+        alert('Make sure you have Metamask')
         return
       } else {
-        console.log("We have the ethereum object", ethereum)
+        console.log('We have the ethereum object', ethereum)
       }
 
-      const accounts = await ethereum.request({ method: "eth_accounts" })
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
 
       if (accounts.length !== 0) {
         const account = account[0]
-        console.log("Found account:", account)
+        console.log('Found account:', account)
         setCurrentAccount(account)
       } else {
-        console.log("No authorize account found!!!!")
+        console.log('No authorize account found!!!!')
       }
     } catch (error) {
       console.log(error)
@@ -34,14 +36,35 @@ const App = () => {
     try {
       const { ethereum } = window
       if (!ethereum) {
-        alert("Get Metamask bby!")
+        alert('Get Metamask bby!')
         return
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
 
-      console.log("Connected", accounts[0])
+      console.log('Connected', accounts[0])
       setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        )
+
+        let count = await wavePortalContract.getTotalWaves()
+        console.log('Retrieved total wave count...', count.toNumber())
+      }
     } catch (error) {
       console.log(error)
     }
@@ -56,10 +79,12 @@ const App = () => {
       <Header />
       <Grid>
         <Main />
-        {!currentAccount && (
+        {!currentAccount ? (
           <Button as="button" onClick={connectWallet}>
             Conectar Wallet
           </Button>
+        ) : (
+          <Button onClick={wave}>Escribe un mensaje</Button>
         )}
       </Grid>
     </>
