041fdf01ecb1dbee6d290540f26743cfe97281f6
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-22 08:54:44 -0500

    Wave Semi Styles

--- src/components/PartnerList.jsx
+++ src/components/PartnerList.jsx
@@ -2,19 +2,32 @@ import { HStack, List, ListItem, Text, Image, Button } from '@chakra-ui/react'
 
 const PartnerList = ({ partnerList }) => {
   return (
-    <List>
+    <List mx="6">
       {partnerList ? (
         partnerList.map((partner, index) => {
           const name = partner[0] ? partner[0] : 'Someone'
           const message = partner[1]
           const gifLink = message.slice(message.length - 3) === 'gif'
           return (
-            <ListItem key={index}>
+            <ListItem
+              key={index}
+              boxShadow="md"
+              px="3"
+              py="2"
+              my="3"
+              border="2px"
+              borderColor="gray.200"
+              borderRadius="xl"
+            >
               <HStack>
-                <Text>{name}</Text>
+                <Text fontWeight="bold">{name}</Text>
                 <Text>Envió un saludo</Text>
               </HStack>
-              {gifLink ? <Image src={message} /> : <Text>{message}</Text>}
+              {gifLink ? (
+                <Image mt="2" src={message} />
+              ) : (
+                <Text mt="2">{message}</Text>
+              )}
             </ListItem>
           )
         })
